a50ff26f9a1f423d7bd1e469d52d0897c784d449
romita kukreja <reemakukreja@gmail.com>
2020-04-12 20:03:37 +0530

    Add changes

--- javaMavenJunit/src/main/java/javaMavenJunit/Add.java
+++ javaMavenJunit/src/main/java/javaMavenJunit/Add.java
@@ -1,13 +1,13 @@
 package javaMavenJunit;
 
 public class Add {
-	
+	/*
 	public int addNumbers(int x, int y) {
 		int sum = x+y;
 		return sum;
-	}
+	}*/
 	
-	public static void main(String [] xargs) {
-		System.out.println("Hi Romita");
+	public static void main(String xargs[]) {
+		System.out.println("Hi Romita hello");
 	}
 }
