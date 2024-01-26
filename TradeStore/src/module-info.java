40600d7bf223dbe8ba2b572600a5091b91360b56
romita kukreja <reemakukreja@gmail.com>
2024-01-26 23:25:32 +0530

    Change-Id: Commit Commit1
    Change-Id: Ib1c89812607e65dcf1ae09e01de016581abb0882
    Signed-off-by: romita kukreja <reemakukreja@gmail.com>

---
+++ TradeStore/src/module-info.java
@@ -0,0 +1,3 @@
+module tradeStore {
+	exports snippet;
+}
---
+++ TradeStore/src/snippet/Snippet.java
@@ -0,0 +1,9 @@
+//test
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+	//	C:\Users\DELL\Git
+	}
+}
+
